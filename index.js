5e303a2b8cf0ea073a8b366bd4613e4ec24db372
atokalov <atokalov@outlook.com>
2016-02-19 18:57:48 +0200

    added openshift environment variables

--- index.js
+++ index.js
@@ -4,7 +4,14 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI;
+var databaseName = '';
+
+if(process.env.OPENSHIFT_APP_NAME) {
+	databaseName = 'parse-server';
+}
+
+var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI 
+	|| process.env.MONGODB_URL + databaseName;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
@@ -31,7 +38,8 @@ app.get('/', function(req, res) {
   res.status(200).send('I dream of being a web site.');
 });
 
-var port = process.env.PORT || 1337;
-app.listen(port, function() {
+var port =  process.env.NODE_PORT || process.env.PORT || 1337;
+var ip = process.env.NODE_IP || process.env.IP || 'localhost';
+app.listen(port, ip, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
