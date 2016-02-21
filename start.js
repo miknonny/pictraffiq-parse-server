930a0ef5bdd91f86462deb8817a6eecefcbb5350
atokalov <atokalov@outlook.com>
2016-02-21 22:20:30 +0200

    switched to multithreaded cluster

--- start.js
+++ start.js
@@ -1,11 +1,12 @@
-const cluster = require('cluster'),
+var cluster = require('cluster'), 
+      os      = require('os'),
       stopSignals = [
         'SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
       ],
       production = process.env.NODE_ENV == 'production';
 
-let stopping = false;
+var stopping = false;
 
 cluster.on('disconnect', function(worker) {
   if (production) {
@@ -17,16 +18,18 @@ cluster.on('disconnect', function(worker) {
   }
 });
 
+var numCPUs = os.cpus().length;
+
 if (cluster.isMaster) {
-  const workerCount = process.env.NODE_CLUSTER_WORKERS || 4;
-  console.log(`Starting ${workerCount} workers...`);
-  for (let i = 0; i < workerCount; i++) {
+  var workerCount = process.env.NODE_CLUSTER_WORKERS || numCPUs || 4;
+  console.log('Starting ' + workerCount + ' workers...');
+  for (var i = 0; i < workerCount; i++) {
     cluster.fork();
   }
   if (production) {
     stopSignals.forEach(function (signal) {
       process.on(signal, function () {
-        console.log(`Got ${signal}, stopping workers...`);
+        console.log('Got ' + signal + ', stopping workers...');
         stopping = true;
         cluster.disconnect(function () {
           console.log('All workers stopped, exiting.');
