8dca0c62d70bcb5cd82423f3e1a3dc92642bd788
nonnycode <miknonny@gmail.com>
2016-03-20 16:33:02 +0100

    added a response.success() to save object

--- cloud/main.js
+++ cloud/main.js
@@ -13,6 +13,14 @@ Parse.Cloud.define('hello', function(request, response) {
 Parse.Cloud.beforeSave('Users', (request, response) => {
   var query = new Parse.Query('Users')
   console.log(request.object.get('email'))
+
+  // Returns an array of objects with email match
+  /**
+   * [ ParseObject { _objCount: 16, className: 'Users', id: 'pQz90ExmeJ' },
+   * ParseObject { _objCount: 17, className: 'Users', id: 'Kxa4KZD4Au' },
+   * ParseObject { _objCount: 18, className: 'Users', id: 'nc6Wf6K0Gj' },
+   * ParseObject { _objCount: 19, className: 'Users', id: 'wdq997Vsvq' } ]
+   */
   query.equalTo('email', request.object.get('email'))
   query.find()
   .then((results) => {
@@ -20,5 +28,5 @@ Parse.Cloud.beforeSave('Users', (request, response) => {
   }, (error) => {
     console.log('error: ', error.code + ' ' + error.message)
   })
-  // response.success()
+  response.success()
 })
