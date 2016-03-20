6bd44802e622c0103123cb1b8490b18723c0ea95
nonnycode <miknonny@gmail.com>
2016-03-20 14:15:51 +0100

    working on Parse.cloud.beforeSave for Users

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,23 @@
+/**
+ * Database Triggers for Pictraffiq.
+ */
 
-Parse.Cloud.define('hello', function(req, res) {
+// Test Code for cloud.
+Parse.Cloud.define('hello', function(request, response) {
   res.success('Hi');
 });
+
+/**
+ * Ensure a single Databse entry for users.
+ */
+Parse.Cloud.beforeSave('Users', (request, response) => {
+  console.log(request.object)
+  query.equalsTo('email', request.object.username)
+  query.find()
+  .then((results) {
+    console.log(results)
+  }, (error) {
+    console.log('error: ', error.code + ' ' + error.message)
+  })
+  // response.success()
+})
